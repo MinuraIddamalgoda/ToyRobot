a96212603ff4205b40918d5d96a99472dc2e7db0
MinuraIddamalgoda <mini2476@gmail.com>
2020-03-15 17:42:02 +1100

    Update main method with new funationality

--- src/main/java/com/ToyRobot/App.java
+++ src/main/java/com/ToyRobot/App.java
@@ -1,23 +1,35 @@
 package com.ToyRobot;
 
+import com.controller.io.CommandParserService;
 import com.controller.io.InputArgParserService;
+import com.model.command.Command;
 import org.apache.commons.cli.ParseException;
 
+import java.io.File;
 import java.io.FileNotFoundException;
+import java.util.List;
 
 public class App {
 
     public static void main(String[] args) {
-        InputArgParserService parserService = new InputArgParserService(args);
-        String filePath = "";
+        InputArgParserService inputArgParserService = new InputArgParserService(args);
+
+        File filePath = null;
 
         try {
-            filePath = parserService.getInputPath();
+            filePath = inputArgParserService.getInputFile();
         } catch (ParseException | FileNotFoundException e) {
             System.err.println(e.getMessage());
             System.exit(1);
         }
 
         System.out.println(filePath);
+
+        CommandParserService commandParserService = new CommandParserService(filePath);
+        List<Command> commandList = commandParserService.getCommands();
+
+        for (Command command: commandList) {
+            System.out.println(command.toString());
+        }
     }
 }
